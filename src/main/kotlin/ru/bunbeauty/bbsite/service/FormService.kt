e3eaf684a7dda60ffc4d2b49c38906fc3d00721f
Valentin Podkopaev <max.astin@yandex.ru>
2019-09-07 15:30:00 +0300

    form template and controllers were created

---
+++ src/main/kotlin/ru/bunbeauty/bbsite/service/FormService.kt
@@ -0,0 +1,12 @@
+package ru.bunbeauty.bbsite.service
+
+import org.springframework.stereotype.Service
+import ru.bunbeauty.bbsite.model.Master
+
+@Service
+class FormService {
+    fun saveData(master: Master) {
+
+    }
+
+}
