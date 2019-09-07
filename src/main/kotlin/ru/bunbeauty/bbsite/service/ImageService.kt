82682662763fbc6202b7e7945b9fd2a3549b51e3
idealik <shavl.mark@yandex.ru>
2019-09-08 02:04:43 +0300

    Create backend form

--- src/main/kotlin/ru/bunbeauty/bbsite/service/ImageService.kt
+++ src/main/kotlin/ru/bunbeauty/bbsite/service/ImageService.kt
@@ -1,11 +1,16 @@
 package ru.bunbeauty.bbsite.service
 
+import com.google.firebase.cloud.StorageClient
 import org.springframework.stereotype.Service
+import java.util.concurrent.TimeUnit
 
 @Service
 class ImageService {
     fun findImageByName(name: String): ByteArray? {
-        return null
+        val imageUrl = StorageClient.getInstance().bucket()[name].signUrl(14,TimeUnit.DAYS)
+        val inputStream = imageUrl.openStream()
+        val byteArr = inputStream.readBytes()
+        inputStream.close()
+        return byteArr
     }
-
 }
