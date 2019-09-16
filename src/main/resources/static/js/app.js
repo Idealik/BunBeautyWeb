b2b7d72deafc3102b22e5a8b89cb0f4e13d6d2dc
Valentin Podkopaev <max.astin@yandex.ru>
2019-09-16 22:42:55 +0300

    design main page and form was updated; animation was added;

---
+++ src/main/resources/static/js/app.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    particlesJS.load('particles-js', '/js/particles-config.json', function() {
+        console.log('callback - app.js config loaded');
+    });
+});
+
